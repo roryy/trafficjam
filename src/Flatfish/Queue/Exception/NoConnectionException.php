669c1afe462a4d209b06fe413859986218f7d0a9
Rory <rory@roryy.com>
2014-12-24 22:05:32 +0100

    exceptions

---
+++ src/Flatfish/Queue/Exception/NoConnectionException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rory
+ * Date: 24-12-14
+ * Time: 17:18
+ */
+
+namespace Flatfish\Queue\Exception;
+
+
+class NoConnectionException extends QueueException {
+
+} 
---
+++ src/Flatfish/Queue/Exception/QueueException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rory
+ * Date: 24-12-14
+ * Time: 17:15
+ */
+
+namespace Flatfish\Queue\Exception;
+
+use \Exception;
+
+class QueueException extends Exception {
+
+} 
