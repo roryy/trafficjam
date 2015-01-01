477f5586a775f541556f88e791f1d950ebd7be17
Rory Scholman <rory@roryy.com>
2015-01-01 21:08:25 +0100

    interface

--- src/Flatfish/Queue/Factory/QueueFactoryInterface.php
+++ src/Flatfish/Queue/Factory/QueueFactoryInterface.php
@@ -10,11 +10,9 @@
 
 namespace Flatfish\Queue\Factory;
 
-use Flatfish\Queue\Connection;
-use Flatfish\Queue\Queue;
 use Flatfish\Queue\QueueAbstract;
 
-class QueueFactory implements QueueFactoryInterface {
+interface QueueFactoryInterface {
 
     /**
      * @param $host
@@ -27,12 +25,6 @@ class QueueFactory implements QueueFactoryInterface {
      * @param bool $durable
      * @return QueueAbstract
      */
-    public static function createQueue($host, $port, $username, $password, $name, $exchange = null, $routingKey = null, $durable = true) {
-        $connection = new Connection($host,$port,$username,$password);
-
-        $queue = new Queue($connection,$name,$exchange,$routingKey,$durable);
-
-        return $queue;
-    }
+    public static function createQueue($host, $port, $username, $password, $name, $exchange = null, $routingKey = null, $durable = true);
 
 } 
